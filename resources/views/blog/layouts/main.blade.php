7ca690727c64e38ed7fcc39a2e04b27d6c90a9af
Jorge Alexis Paz Barrera <jorgepbarrera38@gmail.com>
2018-06-04 15:19:12 -0500

    Modificado .htaccess para redireccionar http a https

--- resources/views/blog/layouts/main.blade.php
+++ resources/views/blog/layouts/main.blade.php
@@ -10,7 +10,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="{{ secure_asset('page/assets/css/main.css') }}" />
+		<link rel="stylesheet" href="{{ asset('page/assets/css/main.css') }}" />
 		@yield('css')
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
@@ -36,7 +36,7 @@
 
 							<!-- Header -->
 								<header id="header">
-									<a href="index.html" class="logo"><strong>Cosas interesantes y curiosidades</strong></a>
+									<a href="{{ route('blog.index') }}" class="logo"><strong>Cosas interesantes y curiosidades</strong></a>
 									<ul class="icons">
 										<li><a href="https://twitter.com/InteresantesCur" target="_blank" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 										<li><a href="https://www.facebook.com/cosasinteresantesycuriosidades1" target="_blank" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
@@ -103,11 +103,11 @@
 			</div>
 
 		<!-- Scripts -->
-			<script src="{{ secure_asset('page/assets/js/jquery.min.js') }}"></script>
-			<script src="{{ secure_asset('page/assets/js/skel.min.js') }}"></script>
-			<script src="{{ secure_asset('page/assets/js/util.js') }}"></script>
+			<script src="{{ asset('page/assets/js/jquery.min.js') }}"></script>
+			<script src="{{ asset('page/assets/js/skel.min.js') }}"></script>
+			<script src="{{ asset('page/assets/js/util.js') }}"></script>
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js') }}"></script><![endif]-->
-			<script src="{{ secure_asset('page/assets/js/main.js') }}"></script>
+			<script src="{{ asset('page/assets/js/main.js') }}"></script>
 			@yield('scripts')
 
 	</body>
